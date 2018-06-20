9a097443698a351057ad61d33f50153b9049b8c1
koyo.arai <koyo.arai@i-studio.co.jp>
2018-06-20 12:06:57 +0900

    added custom button

---
+++ Button_Custom.swift
@@ -0,0 +1,30 @@
+/*
+ 参考:
+ https://iritec.jp/web_service/9167/
+ */
+import UIKit
+
+@IBDesignable
+class Button_Custom: UIButton {
+    
+    @IBInspectable var textColor: UIColor?
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+}
+
